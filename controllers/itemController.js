a0cb537cfcb95d3e9c74feede497d6434e44201c
Mihir Patel <mihir.patel@alphacertlabs.com>
2020-08-25 22:15:34 +1200

    Improved multer error handling

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -5,15 +5,19 @@ const debug = require('debug')('item');
 const fs = require('fs');
 const path = require('path');
 
-const multer = require('multer');
-const upload = multer({
-    dest: 'uploads/', fileFilter: function (_req, file, callback) {
+const multer = require('multer')({
+    dest: 'uploads/',
+
+    // Only allow specific file types to be uploaded
+    fileFilter: function (_req, file, callback) {
         if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg' && file.mimetype !== 'image/gif' && file.mimetype !== 'image/jpeg') {
-            return callback(new Error('Only .png, .jpg, .jpeg & .gif format allowed!'))
+            return callback(new Error('Only .png, .jpg, .jpeg & .gif format allowed'))
         }
         callback(null, true)
-    }, limits: { fileSize: 100000 } // Set file size limit to 100KB
-})
+    },
+    limits: { fileSize: 150000 } // Set file size limit to 150KB
+});
+const image_upload = multer.single('image');
 
 const { body, validationResult } = require('express-validator');
 
@@ -63,9 +67,6 @@ exports.item_create_get = (_req, res, next) => {
 
 exports.item_create_post = [
 
-    // Get image file from request body
-    upload.single('image'),
-
     // Validate & sanitize
     body('name', 'Name must be specified').trim().isLength({ min: 1 }).escape(),
     body('category', 'Category must be specified').trim().isLength({ min: 1 }).escape(),
@@ -98,6 +99,15 @@ exports.item_create_post = [
 
         // Validation errors found
         if (!errors.isEmpty()) {
+
+            // Get all validation errors and convert to an array of error messages
+            let all_errors = errors.array().map(err => err.msg);
+
+            // Get error message from multer error and append to errors list
+            image_upload(req, res, (err) => {
+                if (err) all_errors.push(`Item Image: ${err.message}`);
+            });
+
             // Retrieve all Categories
             Category.find({}, (err, category_list) => {
                 if (err) {
@@ -106,7 +116,7 @@ exports.item_create_post = [
                 }
 
                 // Re-render form with input field's filled in and validation messages showing
-                res.render('item/item_form', { title: 'Create New Item', category_list, selected_category: new_item.category, errors: errors.array(), submitted_item: new_item });
+                res.render('item/item_form', { title: 'Create New Item', category_list, selected_category: new_item.category, errors: all_errors, submitted_item: new_item });
             });
         }
         else {
